585a2652463705fea79b612111166d51955cb89c
Lars Lien <lars@familienlien.com>
2019-04-13 22:11:04 +0200

    adding routing fundamentals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movie from "./components/movie";
+import Rentals from "./components/rentals";
+import NavBar from "./components/navBar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movie />
+        <NavBar />
+        <div>
+          <Switch>
+            <Route path="/movies/:_id?" component={Movie} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
       </main>
     );
   }
