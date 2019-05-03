5d4e4d4463eea12af4a9e2d962f8cf3de0c9641e
LarsL123 <lars@familienlien.com>
2019-05-03 17:44:26 +0200

    Probably the last commit for a while. Now everything is completed and i will start on a backend cours

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,11 +1,10 @@
-import { apiUrl } from "../config.json";
 import httpService from "./httpService.js";
 
 let genres;
 
 export async function getGenres() {
   if (!genres) {
-    const { data } = await httpService.get(apiUrl + "/genres");
+    const { data } = await httpService.get("/genres");
     genres = data;
   }
 
